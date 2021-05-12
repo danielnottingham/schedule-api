6f55eb2f82f8c1c5877d86b449e1a86d57c300f5
Daniel Nottingham <danielnottingham.ti@gmail.com>
2021-05-12 10:23:26 -0300

    override as_json and include accepts attributes for phones

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,11 @@
 class Contact < ApplicationRecord
   belongs_to :kind
   has_many :phones
+  accepts_nasted_attributes_for :phones, allow_destroy: true
+
+  def as_json(options={})
+    attributes = super(options)
+    attributes[:birthdate] = self.birthdate.strftime('%d/%m/%Y')
+    attributes
+  end
 end
